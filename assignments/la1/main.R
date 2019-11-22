7b8df56780cb7d09f5485dd7280dc4d792e016a8
Thomson2412 <thomasm.w.fink@gmail.com>
2019-11-22 12:18:30 +0100

    fix oopsie

--- assignments/la1/main.R
+++ assignments/la1/main.R
@@ -37,10 +37,10 @@ deviationMean <- function(){
     type = keyPressDataWithLaneDeviation$partOfExperiment,
     deveation = abs(keyPressDataWithLaneDeviation$lanePosition)
   )
-  subFrame <- subFrame[subFrame$type == "dualSteerFocus" | subFrame$type == "dualDialFocus",]
+  subFrame <- frame[frame$type == "dualSteerFocus" | frame$type == "dualDialFocus",]
   
-  mean <- aggregate(subFrame$dialingTime, by=list(subFrame$type), FUN=mean)
-  sd <- aggregate(subFrame$dialingTime, by=list(subFrame$type), FUN=sd)
+  mean <- aggregate(subFrame$deveation, by=list(subFrame$type), FUN=mean)
+  sd <- aggregate(subFrame$deveation, by=list(subFrame$type), FUN=sd)
   
   amountOfSteerFocus <- length(subFrame[subFrame$type == "dualSteerFocus", ])
   amountOfDialFocus <- length(subFrame[subFrame$type == "dualDialFocus", ])
@@ -55,6 +55,37 @@ deviationMean <- function(){
     Mean = mean$x,
     SD = sd$x,
     SE = se
+  )
+  return(endFrame)
+}
+
+plotD <- function(){ 
+  frame <- data.frame(
+    participant = keyPressDataWithLaneDeviation$pp,
+    keypress = keyPressDataWithLaneDeviation$phoneNrLengthAfterKeyPress,
+    type = keyPressDataWithLaneDeviation$partOfExperiment,
+    dialingTime = keyPressDataWithLaneDeviation$timeRelativeToTrialStart,
+    error = keyPressDataWithLaneDeviation$typingErrorMadeOnTrial
+  )
+  
+  subFrame <- frame[frame$keypress == 11,]
+  subFrame <- subFrame[subFrame$error == 0,]
+  subFrame <- subFrame[subFrame$type == "dualSteerFocus" | subFrame$type == "dualDialFocus",]
+  
+  mean <- aggregate(subFrame$dialingTime, by=list(subFrame$type), FUN=mean)
+  
+  sd <- aggregate(subFrame$dialingTime, by=list(subFrame$type), FUN=sd)
+  
+  amountOfParticipants <- length(unique(frame$participant))
+  print(amountOfParticipants)
+  
+  se <- sd$x / sqrt(amountOfParticipants)
+  
+  endFrame <- data.frame(
+    Type = unique(subFrame$type),
+    Mean = round(mean$x / 1000, digits = 1),
+    SD = round(sd$x / 1000, digits = 1),
+    SE = round(se / 1000, digits = 1)
   )
   return(endFrame)
 }
