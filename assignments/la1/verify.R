a21f807da1ba3a0db21ab8c19bc8f29c152d82b5
Zhao Shu <zhaobenben007@googlemail.com>
2019-11-23 15:43:09 +0100

    add verify

--- assignments/la1/verify.R
+++ assignments/la1/verify.R
@@ -1,54 +1,128 @@
-load("keyPressDataWithLaneDeviation.Rdata")
+library(ggplot2)
 
-dialingTime <- function() {
-  frame <- data.frame(
-    participant = keyPressDataWithLaneDeviation$pp,
-    keypress = keyPressDataWithLaneDeviation$phoneNrLengthAfterKeyPress,
-    type = keyPressDataWithLaneDeviation$partOfExperiment,
-    dialingTime = keyPressDataWithLaneDeviation$timeRelativeToTrialStart,
-    error = keyPressDataWithLaneDeviation$typingErrorMadeOnTrial
-  )
+dialingTimeV1 <- function() {
   
-  mean <- with(frame[(frame$keypress == 11) & (frame$error == 0) & (frame$type == "dualSteerFocus" | frame$type == "dualDialFocus"),], aggregate(dialingTime, list(Type=type), mean))
-  sd <- with(frame[(frame$keypress == 11) & (frame$error == 0) & (frame$type == "dualSteerFocus" | frame$type == "dualDialFocus"),], aggregate(dialingTime, list(Type=type), sd))
-  se <- sd / sqrt(length(unique(frame$participant)))
+  frame <- read.csv('keyPressDataWithLaneDeviation.csv', header = TRUE)
   
-  endFrame <- data.frame(
-    Type = c('Dual dial ocus', 'Dual steer focus'),
+  frame <- frame[(frame$Event1 == 'Correct') 
+                 & (frame$typingErrorMadeOnTrial == 0) 
+                 & (frame$partOfExperiment == "dualSteerFocus" | frame$partOfExperiment == "dualDialFocus"),]
+  
+  mean <- with(frame, aggregate(timeRelativeToTrialStart, list(Type=partOfExperiment), mean))
+  sd <- with(frame, aggregate(timeRelativeToTrialStart, list(Type=partOfExperiment), sd))
+  se <- sd$x / sqrt(length(unique(frame$pp)))
+  
+  resultFrame <- data.frame(
+    Type = mean$Type,
     Mean = round(mean$x / 1000, digits = 1),
     SD = round(sd$x / 1000, digits = 1),
-    SE = round(se$x / 1000, digits = 1)
+    SE = round(se / 1000, digits = 1)
   )
-  
-  endFrame
+  resultFrame
 }
 
-lateralDeviation <- function() {
-  frame <- data.frame(
-    participant = keyPressDataWithLaneDeviation$pp,
-    event1 = keyPressDataWithLaneDeviation$Event1,
-    type = keyPressDataWithLaneDeviation$partOfExperiment,
-    position = keyPressDataWithLaneDeviation$lanePosition,
-    error = keyPressDataWithLaneDeviation$typingErrorMadeOnTrial
+print(dialingTimeV1())
+
+dialingTimeV2 <- function() {
+  
+  frame <- read.csv('keyPressDataWithLaneDeviation.csv', header = TRUE)
+  
+  frame <- frame[(frame$Event1 == 'Correct') 
+                 & (frame$typingErrorMadeOnTrial == 0) 
+                 & (frame$partOfExperiment == "dualSteerFocus" | frame$partOfExperiment == "dualDialFocus"),]
+  
+  mean <- with(frame, aggregate(timeRelativeToTrialStart, list(Type=partOfExperiment), mean))
+  sd <- with(frame, aggregate(timeRelativeToTrialStart, list(Type=partOfExperiment), sd))
+  se <- c(0, 0)
+  
+  for(pp in seq(length(unique(frame$pp)))) {
+    framePP <- frame[frame$pp == pp,]
+    meanPP <- with(framePP, aggregate(timeRelativeToTrialStart, list(Type=partOfExperiment), mean))
+    se <- (meanPP$x - mean$x)**2 + se
+  }
+  
+  se <- sqrt(se/(length(unique(frame$pp)) - 1))
+  
+  resultFrame <- data.frame(
+    Type = mean$Type,
+    Mean = round(mean$x / 1000, digits = 1),
+    SD = round(sd$x / 1000, digits = 1),
+    SE = round(se / 1000, digits = 1)
   )
+  resultFrame
+}
+
+print(dialingTimeV2())
+
+lateralDeviationV1 <- function() {
   
-  frame1 <- frame[(frame$event1 == 'Start') & (frame$error == 0) & (frame$type == "dualSteerFocus" | frame$type == "dualDialFocus"),]
-  frame2 <- frame[(frame$event1 == 'Correct') & (frame$error == 0) & (frame$type == "dualSteerFocus" | frame$type == "dualDialFocus"),]
-  lateralDeviation <- frame2$position - frame1$position
-  endFrame <- cbind(frame1, lateralDeviation)
+  frame <- read.csv('keyPressDataWithLaneDeviation.csv', header = TRUE)
   
-  mean <- with(endFrame, aggregate(lateralDeviation, list(Type=type), mean))
-  sd <- with(endFrame, aggregate(lateralDeviation, list(Type=type), sd))
-  se <- sd$x / sqrt(length(unique(frame$participant)))
+  startFrame <- frame[(frame$Event1 == 'Start')
+                 & (frame$typingErrorMadeOnTrial == 0) 
+                 & (frame$partOfExperiment == "dualSteerFocus" | frame$partOfExperiment == "dualDialFocus"),]
   
-  endFrame <- data.frame(
-    Type = c('Dual dial ocus', 'Dual steer focus'),
+  endFrame <- frame[(frame$Event1 == 'Correct')
+                      & (frame$typingErrorMadeOnTrial == 0) 
+                      & (frame$partOfExperiment == "dualSteerFocus" | frame$partOfExperiment == "dualDialFocus"),]
+  
+  steps <- unique(endFrame$phoneNrLengthAfterKeyPress)
+  lateralDeviation <- abs((endFrame$lanePosition * 1000 - startFrame$lanePosition * 1000) / steps)
+  combinedFrame <- cbind(startFrame, lateralDeviation)
+  #View(combinedFrame)
+  
+  mean <- with(combinedFrame, aggregate(lateralDeviation, list(Type=partOfExperiment), mean))
+  sd <- with(combinedFrame, aggregate(lateralDeviation, list(Type=partOfExperiment), sd))
+  se <- sd$x / sqrt(length(unique(combinedFrame$pp)))
+  
+  resultFrame <- data.frame(
+    Type = mean$Type,
     Mean = mean$x,
     SD = sd$x,
     SE = se
   )
   
-  endFrame
+  resultFrame
 }
 
-lateralDeviation()
+print(lateralDeviationV1())
+
+lateralDeviationV2 <- function() {
+  
+  frame <- read.csv('keyPressDataWithLaneDeviation.csv', header = TRUE)
+  
+  startFrame <- frame[(frame$Event1 == 'Start')
+                      & (frame$typingErrorMadeOnTrial == 0) 
+                      & (frame$partOfExperiment == "dualSteerFocus" | frame$partOfExperiment == "dualDialFocus"),]
+  
+  endFrame <- frame[(frame$Event1 == 'Correct')
+                    & (frame$typingErrorMadeOnTrial == 0) 
+                    & (frame$partOfExperiment == "dualSteerFocus" | frame$partOfExperiment == "dualDialFocus"),]
+  
+  steps <- unique(endFrame$phoneNrLengthAfterKeyPress)
+  lateralDeviation <- abs((endFrame$lanePosition * 1000 - startFrame$lanePosition * 1000) / steps)
+  combinedFrame <- cbind(startFrame, lateralDeviation)
+  
+  mean <- with(combinedFrame, aggregate(lateralDeviation, list(Type=partOfExperiment), mean))
+  sd <- with(combinedFrame, aggregate(lateralDeviation, list(Type=partOfExperiment), sd))
+  se <- c(0, 0)
+  
+  for(pp in seq(length(unique(combinedFrame$pp)))) {
+    framePP <- combinedFrame[combinedFrame$pp == pp,]
+    meanPP <- with(framePP, aggregate(lateralDeviation, list(Type=partOfExperiment), mean))
+    se <- (meanPP$x - mean$x)**2 + se
+  }
+  
+  se <- sqrt(se/(length(unique(combinedFrame$pp)) - 1))
+  
+  resultFrame <- data.frame(
+    Type = mean$Type,
+    Mean = mean$x,
+    SD = sd$x,
+    SE = se
+  )
+  
+  resultFrame
+}
+
+print(lateralDeviationV2())
